8af3f6882454fc418f0c42167cfe727d90f760e9
MisakaCirno <misakacirno@qq.com>
2024-07-26 01:58:10 +0800

    更新

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// 会刷新fate的地图
+/**
+ * 会刷新Fate的地图数据
+ */
 const fateDataMap =
 {
     "拉诺西亚": ["中拉诺西亚", "拉诺西亚低地", "东拉诺西亚", "西拉诺西亚", "拉诺西亚高地", "拉诺西亚外地"],
@@ -21,41 +23,107 @@ const fateDataMap =
     "其它": ["季节活动", "重建伊修加德", "成就危命一览"]
 };
 
-// 联动的原始数据
-const sourceData =
-{
-    "战士": { "宠物": "地缚猫", "地区": ["黑衣森林中央林区", "拉诺西亚低地", "中萨纳兰"] },
-    "白魔法师": { "宠物": "小狛", "地区": ["黑衣森林东部林区", "西萨纳兰", "东萨纳兰"] },
-    "吟游诗人": { "宠物": "维斯帕", "地区": ["黑衣森林南部林区", "拉诺西亚高地", "南萨纳兰"] },
-    "黑魔法师": { "宠物": "吹雪公主", "地区": ["黑衣森林北部林区", "拉诺西亚外地", "中拉诺西亚"] },
-    "忍者": { "宠物": "九尾", "地区": ["西萨纳兰", "黑衣森林中央林区", "拉诺西亚低地"] },
-    "学者": { "宠物": "狛次郎", "地区": ["中萨纳兰", "黑衣森林东部林区", "西萨纳兰"] },
-    "召唤师": { "宠物": "人面犬", "地区": ["东萨纳兰", "黑衣森林南部林区", "拉诺西亚高地"] },
-    "占星术士": { "宠物": "野槌蛇", "地区": ["南萨纳兰", "黑衣森林北部林区", "拉诺西亚外地"] },
-    "龙骑士": { "宠物": "大蛇", "地区": ["中拉诺西亚", "西萨纳兰", "黑衣森林中央林区"] },
-    "骑士": { "宠物": "武士猫", "地区": ["拉诺西亚低地", "中萨纳兰", "黑衣森林东部林区"] },
-    "暗黑骑士": { "宠物": "浮游猫", "地区": ["西拉诺西亚", "黑衣森林南部林区", "东萨纳兰"] },
-    "机工士": { "宠物": "机器猫F型", "地区": ["拉诺西亚高地", "南萨纳兰", "黑衣森林北部林区"] },
-    "武僧": { "宠物": "USA蹦", "地区": ["拉诺西亚外地", "中拉诺西亚", "西萨纳兰"] },
-    "绝枪战士": { "宠物": "阎魔", "地区": ["基拉巴尼亚边区", "红玉海", "延夏", "基拉巴尼亚山区", "基拉巴尼亚湖区", "太阳神草原"] },
-    "武士": { "宠物": "滑头鬼", "地区": ["库尔札斯西部高地", "龙堡参天高地", "龙堡内陆低地", "翻云雾海", "阿巴拉提亚云海", "魔大陆阿济兹拉"] },
-    "赤魔法师": { "宠物": "蛇王凯拉", "地区": ["库尔札斯西部高地", "龙堡参天高地", "龙堡内陆低地", "翻云雾海", "阿巴拉提亚云海", "魔大陆阿济兹拉"] },
-    "舞者": { "宠物": "百鬼公主", "地区": ["基拉巴尼亚边区", "红玉海", "延夏", "基拉巴尼亚山区", "基拉巴尼亚湖区", "太阳神草原"] }
-};
 
-const jobToMapName = {};
+/**
+ * 联动的原始数据
+ */
+const yokaiSourceData = [
+    // T
+    { "job": "骑士", "pet": "武士猫", "regions": ["拉诺西亚低地", "中萨纳兰", "黑衣森林东部林区"] },
+    { "job": "战士", "pet": "地缚猫", "regions": ["黑衣森林中央林区", "拉诺西亚低地", "中萨纳兰"] },
+    { "job": "暗黑骑士", "pet": "浮游猫", "regions": ["西拉诺西亚", "黑衣森林南部林区", "东萨纳兰"] },
+    { "job": "绝枪战士", "pet": "阎魔", "regions": ["基拉巴尼亚边区", "红玉海", "延夏", "基拉巴尼亚山区", "基拉巴尼亚湖区", "太阳神草原"] },
+    // D
+    { "job": "武僧", "pet": "USA蹦", "regions": ["拉诺西亚外地", "中拉诺西亚", "西萨纳兰"] },
+    { "job": "龙骑士", "pet": "大蛇", "regions": ["中拉诺西亚", "西萨纳兰", "黑衣森林中央林区"] },
+    { "job": "忍者", "pet": "九尾", "regions": ["西萨纳兰", "黑衣森林中央林区", "拉诺西亚低地"] },
+    { "job": "武士", "pet": "滑头鬼", "regions": ["库尔札斯西部高地", "龙堡参天高地", "龙堡内陆低地", "翻云雾海", "阿巴拉提亚云海", "魔大陆阿济兹拉"] },
+    // 远敏
+    { "job": "吟游诗人", "pet": "维斯帕", "regions": ["黑衣森林南部林区", "拉诺西亚高地", "南萨纳兰"] },
+    { "job": "机工士", "pet": "机器猫F型", "regions": ["拉诺西亚高地", "南萨纳兰", "黑衣森林北部林区"] },
+    { "job": "舞者", "pet": "百鬼公主", "regions": ["基拉巴尼亚边区", "红玉海", "延夏", "基拉巴尼亚山区", "基拉巴尼亚湖区", "太阳神草原"] },
+    // 法系
+    { "job": "黑魔法师", "pet": "吹雪公主", "regions": ["黑衣森林北部林区", "拉诺西亚外地", "中拉诺西亚"] },
+    { "job": "召唤师", "pet": "人面犬", "regions": ["东萨纳兰", "黑衣森林南部林区", "拉诺西亚高地"] },
+    { "job": "赤魔法师", "pet": "蛇王凯拉", "regions": ["库尔札斯西部高地", "龙堡参天高地", "龙堡内陆低地", "翻云雾海", "阿巴拉提亚云海", "魔大陆阿济兹拉"] },
+    // H
+    { "job": "白魔法师", "pet": "小狛", "regions": ["黑衣森林东部林区", "西萨纳兰", "东萨纳兰"] },
+    { "job": "学者", "pet": "狛次郎", "regions": ["中萨纳兰", "黑衣森林东部林区", "西萨纳兰"] },
+    { "job": "占星术士", "pet": "野槌蛇", "regions": ["南萨纳兰", "黑衣森林北部林区", "拉诺西亚外地"] }
+]
 
-// Traverse the input JSON
-for (const [job, { regions }] of Object.entries(inputJson)) {
-    for (const regionName of regions) {
-        // If the region name does not exist in the output JSON, initialize it as an array
-        if (!jobToMapName[regionName]) {
-            jobToMapName[regionName] = [];
+const tempMap = {};
+
+for (const data of yokaiSourceData) {
+    const regions = data.regions;
+
+    regions.forEach(region => {
+        if (!tempMap[region]) {
+            tempMap[region] = [];
         }
-        // Add the job to the corresponding region's array
-        jobToMapName[regionName].push(job);
-    }
+        tempMap[region].push(data.job);
+    });
 }
 
-console.log(JSON.stringify(jobToMapName, null, 4));
+const yokaiJobToMapData = [];
+for (const map in tempMap) {
+    let data = {
+        map: map,
+        jobs: tempMap[map]
+    }
+    yokaiJobToMapData.push(data);
+}
 
+/*
+console.log(fateDataMap);
+console.log(yokaiSourceData);
+console.log(tempMap);
+console.log(yokaiJobToMapData);
+*/
+
+var app = Vue.createApp({
+    data() {
+        let datas = {
+            highlightedRegion: '',
+
+            job2map: yokaiSourceData,
+
+            highlightedJob: '',
+
+            map2job: yokaiJobToMapData,
+        }
+
+        return datas;
+    },
+    methods: {
+        highlightRegion(region) {
+            this.highlightedRegion = region;
+        },
+        highlightJob(job) {
+            this.highlightedJob = job;
+        },
+
+        showTooltip(event) {
+            const tooltip = document.getElementById('tooltip');
+
+            // 设置tooltip中imager的src
+            const job = event.target.getAttribute('job');
+
+            const imager = document.getElementById('imager');
+            imager.src = `images/weapons/${job}.jpg`;
+
+            tooltip.style.display = 'block';
+            tooltip.style.left = event.pageX + 'px';
+            tooltip.style.top = event.pageY + 'px';
+        },
+        hideTooltip() {
+            const tooltip = document.getElementById('tooltip');
+            tooltip.style.display = 'none';
+        },
+
+    }
+})
+
+app.mount('#app')
+
+console.log(app);
